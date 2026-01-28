46ca8ee87afd41b1c4330b3be66465ac7afb33ce
Fajar Sya <fajarsyaa@gmail.com>
2026-01-28 21:13:55 +0700

    Implement Gemini AI API endpoints

---
+++ index.js
@@ -0,0 +1,88 @@
+import { GoogleGenAI } from "@google/genai";
+import express from "express";
+import multer from "multer";
+import 'dotenv/config';
+
+const app = express();
+const upload = multer();
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+const GEMINI_MODEL = "gemini-2.5-flash";
+
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.post("/generate-text", async (req, res) => {
+    const { prompt } = req.body;
+    try {
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents: prompt
+        });
+        res.json({ response: response.text });
+    } catch (error) {
+        console.log(error);
+        
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.post("/generate-from-image", upload.single("image"), async (req, res) => {
+    const { prompt } = req.body;
+    const base64Image = req.file.buffer.toString("base64");
+
+    try {
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents: [
+                {text: prompt,type: "text"},                
+                {inlineData: {data: base64Image,mimeType: req.file.mimetype}}
+            ]
+        });
+
+        res.json({ response: response.text });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.post("/generate-from-document", upload.single("document"), async (req, res) => {
+    const { prompt } = req.body;
+    const base64Document = req.file.buffer.toString("base64");
+
+    try {
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents: [
+                {text: prompt ?? "buatkan rangkuman dokumen",type: "text"},                
+                {inlineData: {data: base64Document,mimeType: req.file.mimetype}}
+            ]
+        });
+        res.json({ response: response.text });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.post("/generate-from-audio", upload.single("audio"), async (req, res) => {
+    const { prompt } = req.body;
+    const base64Audio = req.file.buffer.toString("base64");
+    try {
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents: [
+                {text: prompt ?? "buatkan transkripsi audio",type: "text"},                
+                {inlineData: {data: base64Audio,mimeType: req.file.mimetype}}
+            ]
+        });
+        res.json({ response: response.text });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+});
